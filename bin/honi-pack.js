304c258e12df37e9e6e91bd5708b0a427ada546f
Honi <1095175021@qq.com>
2023-10-22 21:39:13 +0800

    init: 项目初始化

---
+++ bin/honi-pack.js
@@ -0,0 +1,7 @@
+#! /usr/bin/env node
+// Hashbang 注释, 当在 shell 中运行时, 指定 shell 去调用解释器
+// 注意路径是 /usr 而不是 /user
+console.log('start')
+
+// 需要在根目录执行 npm link, 将这个包链接到全局上, 等同于 npm i -g
+// 在需要 my-pack 命令的仓库, 执行 npm link my-pack 进行链接
